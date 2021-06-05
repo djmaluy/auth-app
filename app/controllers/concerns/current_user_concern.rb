f47844ef7b33b372c1ad949e4a04e06151057d03
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-06-05 08:20:28 +0300

    implemented the Registration Controller

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,13 @@
+module CurrentUserConsern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_current_user
+  end
+
+  def set_scurrent_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+end
